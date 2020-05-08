d092cdc78e9784247b40d5a5e333a311dc4fe9da
chaohui <chaohui@qq.com>
2020-05-08 14:34:57 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 
-export default class App extends Component{
-    render(){
-        return(
-            <div>App</div>
-        )
-    }
+import { BrowserRouter,HashRouter ,Switch,Route} from "react-router-dom";
+import Login from './pages/login/Login';
+import Admin from './pages/admin/Admin';
+/*
+应用根组件
+ */
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Admin} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
+
+export default App
+
